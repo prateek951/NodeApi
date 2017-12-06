8f5c17fb247172b7d34326e8540289c710624098
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-12-07 00:25:32 +0530

    added morgan&errorhandling

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const products = require('./api/routes/products');
 const orders = require('./api/routes/orders');
 
+app.use(morgan('dev'));
 app.use('/products',products);
 app.use('/orders',orders);
+
+app.use((req,res,next)=>{
+	const error = new Error('Not Found!!');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error,req,res,next)=>{
+	res.status(error.status || 500).json({
+		error : {
+			message : error.message
+		}
+	});
+});
+
 
 module.exports = app;
