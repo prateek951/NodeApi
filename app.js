a7209bb519335f0a5b9f356cefb4c1a67438928e
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-12-09 13:55:42 +0530

    cors and body-parser added!!

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+//CROSS ORIGIN RESOURCE SHARING
+const cors = require('cors');
+
 const products = require('./api/routes/products');
 const orders = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+	res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+
+	if(req.method === 'OPTIONS'){
+		res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,GET,DELETE');
+		return res.status(200).json({});
+	}
+	next();
+});
+
 app.use('/products',products);
 app.use('/orders',orders);
 
