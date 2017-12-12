8096e559b5d0661fa9a92edafd9fb952f2ef6985
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-12-13 01:35:25 +0530

    added promises in products routes!

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,12 +9,17 @@ router.get('/',(req,res,next)=>{
 
 });
 
-
+//CREATING AN ORDER
 router.post('/',(req,res,next)=>{
 
+	const order = {
+		productId : req.body.productId,
+		quantity : req.body.quantity
+	}
 	res.status(200).json({
-		message : 'Order was created!!'
-	})
+		message : 'Order was created',
+		order
+	});
 
 });
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,60 +1,104 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
-
+const Product = require('../models/product');
 //GET THE LIST OF ALL THE PRODUCTS
 router.get('/',(req,res,next)=>{
 
-	res.status(200).json({
-		message : 'Hey it works!'
+	Product.find({}).exec().then(results => {
+		console.log(results);
+		// if(results.length>=0){
+			res.status(200).json(results);
+		// }
+		// else {
+			// res.status(404).json({message : 'No valid entries are found to exist!'});
+		// }
 	})
+	.catch(err => {
+		console.log(err)
+		res.status(500).json({error : err});
+	});
 
 });
-//ADD A PRODUCT TO THE CART
+//ADD A PRODUCT TO THE SHOPPPING CART
 router.post('/',(req,res,next)=>{
 
-	res.status(200).json({
-		message : 'Hey it works'
-	})
+	//create the product instance with the details that the client provided
+	const product = new Product({
+		_id : new mongoose.Types.ObjectID,
+		name : req.body.name,
+		price : req.body.price
+	});
 
+	post.save().then(result => {
+		console.log(result)
+		res.status(201).json({
+			message : 'Handling POST requests to /products',
+			createdProduct : product
+	});
+
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({error : err});
+	});
 });
-
 //RETRIEVE A SPECIFIC PRODUCT 
 router.get('/:id',(req,res,next)=>{
 
 	const id = req.params.id;
-	//PERFORM A SIMPLE CHECK FOR IF ID = special
-	if(id === 'special'){
 
-		res.status(200).json({
-			message: 'Hey it works!!!'
-		})
-	}
-	else {
+	Product.findById(id).exec().then(doc => {
+		console.log(doc);
+		if(doc){
+			res.status(200).json(doc);
+		}
+		else {
+			res.status(404).json({message : 'No valid ID found!!'});
+		}
+	})
+	.catch(err => {
+		console.log(err)
+		res.status(500).json({error : err})
+	});
 
-		res.status(200).json({
-			message: 'This is not special!!'
-		})
-
-	}
 });
 
 //PATCH REQUEST TO UPDATE A PART OF SOME PRODUCT
 
 router.patch('/:id',(req,res,next)=>{
+	
+	//Simple object keeps track of the update operations
+	const updateOps  = {};
+	//Loop over the req.body
+	for(const ops of req.body){
+		updateOps[ops.propName] = ops.value;
+	}
 
-	res.status(200).json({
-		message: 'Hey it works!! Updated the product!'
-	})
+	Product.findByIdAndUpdate(req.params.id,{ $set : updateOps})
+		.exec().then(result => {
+			console.log(result);
+			res.status(200).json(result);
+		}).catch(err => {
+			console.log(err);
+			res.status(500).json({error : err});
+		});
+
 });
 
 
 //DELETE THE PRODUCT WITH SPECIFIC ID
 
 router.delete('/:id',(req,res,next)=>{
-
-	res.status(200).json({
-		message : 'Hey it works!! Deleted The product!!'
+	const id = req.params.id
+	Product.remove({_id : id}).exec().then(result => {
+		console.log(result);
+		res.status(200).json(result);
 	})
+	.catch(err => {
+		console.log(err);
+		res.status.json({error : err});
+	});
+
 });
 
 module.exports= router;
